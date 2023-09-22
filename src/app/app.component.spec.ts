86fd017c527c69c1bcc73ee97ba2a4a0f278743f
Johan-Ortega811 <masterchief_2001118@hotmail.com>
2023-09-21 20:42:10 -0600

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('debe crear app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`debe crear el titulo'FormulaPeso'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('FormulaPeso');
+  });
+
+  it('debe mostrar texto e imagen', () => {
+    const app =AppComponent;
+    expect(app).toBeTruthy(); 
+  });
+});
